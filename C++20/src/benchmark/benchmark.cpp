7ea99dcb0d1329ca70c8fd7511ac042b23affc52
Matti Lehtonen <m-matti-a.lehtonen@iki.fi>
2023-02-15 21:53:05 +0200

    Rename benchmark base functions

--- C++20/src/benchmark/benchmark.cpp
+++ C++20/src/benchmark/benchmark.cpp
@@ -33,7 +33,7 @@ static inline std::array<DataType, N> constexpr BM_setup2()
 //
 
 template <class BS, char const* NAME>
-static inline void BM_testcase(benchmark::State& state)
+static inline void BM_dynamic_testcase(benchmark::State& state)
 {
     std::vector<DataType> const bench = BM_setup(bench_size);
     for (auto _ : state) {
@@ -45,11 +45,35 @@ static inline void BM_testcase(benchmark::State& state)
 }
 
 template <class BS, char const* NAME>
-static inline void BM_testcase2(benchmark::State& state)
+static inline void BM_static_testcase(benchmark::State& state)
 {
     std::array<DataType, bench_size> const bench = BM_setup2<bench_size>();
     for (auto _ : state) {
         if (!test(BS {}, bench)) {
+            std::cerr << NAME << " failed!" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+    }
+}
+
+template <class BS, char const* NAME>
+static inline void BM_dynamic_testcase_indexless(benchmark::State& state)
+{
+    std::vector<DataType> const bench = BM_setup(bench_size);
+    for (auto _ : state) {
+        if (!test_indexless(BS {}, bench)) {
+            std::cerr << NAME << " failed!" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+    }
+}
+
+template <class BS, char const* NAME>
+static inline void BM_static_testcase_indexless(benchmark::State& state)
+{
+    std::array<DataType, bench_size> const bench = BM_setup2<bench_size>();
+    for (auto _ : state) {
+        if (!test_indexless(BS {}, bench)) {
             std::cerr << NAME << " failed!" << std::endl;
             std::exit(EXIT_FAILURE);
         }
@@ -63,37 +87,37 @@ static inline void BM_testcase2(benchmark::State& state)
 static void BM_traditional1(benchmark::State& state)
 {
     static char constexpr name[] = "traditional1";
-    BM_testcase<traditional1<DataType>, name>(state);
+    BM_dynamic_testcase_indexless<traditional1<DataType>, name>(state);
 }
 
 static void BM_traditional2(benchmark::State& state)
 {
     static char constexpr name[] = "traditional2";
-    BM_testcase<traditional2<DataType>, name>(state);
+    BM_dynamic_testcase_indexless<traditional2<DataType>, name>(state);
 }
 
 static void BM_alternative(benchmark::State& state)
 {
     static char constexpr name[] = "alternative";
-    BM_testcase<alternative<DataType>, name>(state);
+    BM_dynamic_testcase_indexless<alternative<DataType>, name>(state);
 }
 
 static void BM_range(benchmark::State& state)
 {
     static char constexpr name[] = "range";
-    BM_testcase<range<DataType>, name>(state);
+    BM_dynamic_testcase_indexless<range<DataType>, name>(state);
 }
 
 static void BM_power_dynamic(benchmark::State& state)
 {
     static char constexpr name[] = "power<dynamic>";
-    BM_testcase<power<DataType>, name>(state);
+    BM_dynamic_testcase_indexless<power<DataType>, name>(state);
 }
 
 static void BM_power_static(benchmark::State& state)
 {
     static char constexpr name[] = "power<static>";
-    BM_testcase2<power<DataType>, name>(state);
+    BM_static_testcase_indexless<power<DataType>, name>(state);
 }
 
 BENCHMARK(BM_traditional1);
