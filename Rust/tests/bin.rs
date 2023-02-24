c8098bba04b191f3eeb80c01fb35b226ab56c360
Matti Lehtonen <m-matti-a.lehtonen@iki.fi>
2023-02-24 17:37:49 +0200

    Initial dynamic unit tests

--- Rust/tests/bin.rs
+++ Rust/tests/bin.rs
@@ -3,6 +3,7 @@ extern crate bs;
 pub mod testing;
 pub mod utility_tests;
 pub mod helpers;
+pub mod dynamic_tests;
 
 #[test]
 fn utility_tests()
@@ -45,6 +46,25 @@ fn utility_tests()
     tests4.run_all_tests();
 }
 
+#[test]
+fn binary_search_dynamic_tests()
+{
+    use testing::parameterizedtest::{FrameWorkFixture, FrameWorkTrait};
+    use dynamic_tests::BinarySearchDynamicTests;
+    use dynamic_tests::TESTFIXTURE;
+    use dynamic_tests::{DATASET5};
+
+    let mut tests5: BinarySearchDynamicTests = BinarySearchDynamicTests {
+        framework_fixture: FrameWorkFixture {
+            test_fixture: TESTFIXTURE,
+            dataset_fixture: DATASET5,
+        },
+        argument: vec![],
+    };
+    tests5.run_all_tests();
+}
+
 pub fn main() {
     utility_tests();
+    binary_search_dynamic_tests();
 }
---
+++ Rust/tests/dynamic_tests/mod.rs
@@ -0,0 +1,114 @@
+use crate::testing::matcher::assert_true;
+use crate::testing::parameterizedtest::{FrameWorkFixture, FrameWorkTrait};
+use crate::testing::TestResult;
+use super::helpers::Dynamic;
+
+type DataType = i32;
+type SignedIndexType = isize;
+type IndexType = usize;
+
+type TestData = usize;
+
+#[derive(Clone)]
+pub struct BinarySearchDynamicTests {
+    pub framework_fixture: FrameWorkFixture<Self, TestData>,
+    pub argument: Vec<DataType>,
+}
+
+impl FrameWorkTrait<BinarySearchDynamicTests, TestData> for BinarySearchDynamicTests {
+    fn setup(&mut self, param: &TestData) {
+        self.argument = Dynamic::filler::<DataType, SignedIndexType>(param);
+    }
+
+    fn get_fixture(&self) -> &Self {
+        return self;
+    }
+
+    fn get_framework_fixture(&self) -> &FrameWorkFixture<Self, TestData> {
+        return &self.framework_fixture;
+    }
+}
+
+use bs::binary_search::{alternative, power, range, traditional};
+
+impl BinarySearchDynamicTests {
+    fn signed_traditional(fixture:&BinarySearchDynamicTests, _param: &TestData) -> TestResult {
+        let bs: traditional::SignedImplementation = traditional::SignedImplementation {};
+
+        let result: bool = Dynamic::test::<DataType, SignedIndexType>(&bs, &fixture.argument);
+        return assert_true(result);
+    }
+
+    fn unsigned_traditional(fixture:&BinarySearchDynamicTests, _param: &TestData) -> TestResult {
+        let bs: traditional::UnsignedImplementation =
+            traditional::UnsignedImplementation {};
+
+        let result: bool = Dynamic::test::<DataType, IndexType>(&bs, &fixture.argument);
+        return assert_true(result);
+    }
+
+    fn alternative(fixture:&BinarySearchDynamicTests, _param: &TestData) -> TestResult {
+        let bs: alternative::Implementation = alternative::Implementation {};
+
+        let result: bool = Dynamic::test::<DataType, IndexType>(&bs, &fixture.argument);
+        return assert_true(result);
+    }
+
+    fn range(fixture:&BinarySearchDynamicTests, _param: &TestData) -> TestResult {
+        let bs: range::Implementation = range::Implementation {};
+
+        let result: bool = Dynamic::test::<DataType, IndexType>(&bs, &fixture.argument);
+        return assert_true(result);
+    }
+
+    fn power(fixture:&BinarySearchDynamicTests, _param: &TestData) -> TestResult {
+        let bs: power::DynamicImplementation = power::DynamicImplementation {};
+
+        let result: bool = Dynamic::test::<DataType, IndexType>(&bs, &fixture.argument);
+        return assert_true(result);
+    }
+}
+
+use crate::testing::parameterizedtest::{DataSetFixture, TestCase, TestFixture};
+
+pub const TESTFIXTURE: TestFixture<BinarySearchDynamicTests, TestData> = TestFixture {
+    name: "BinarySearchDynamicTests",
+    tests: &[
+        TestCase{
+            name: "signed_traditional",
+            test: &BinarySearchDynamicTests::signed_traditional,
+        },
+        TestCase{
+            name: "unsigned_traditional",
+            test: &BinarySearchDynamicTests::unsigned_traditional,
+        },
+        TestCase{
+            name: "alternative",
+            test: &BinarySearchDynamicTests::alternative,
+        },
+        TestCase{
+            name: "range",
+            test: &BinarySearchDynamicTests::range,
+        },
+        TestCase{
+            name: "power",
+            test: &BinarySearchDynamicTests::power,
+        },
+    ],
+};
+
+pub const DATASET5: DataSetFixture<TestData> = DataSetFixture{
+    name: "ArraySizes",
+    dataset: &[
+        0,
+        1,
+        2,
+        3,
+        10,
+        100,
+        511,
+        512,
+        513,
+        1 << 16,
+    ],
+};
